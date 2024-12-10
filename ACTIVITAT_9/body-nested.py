592d7b70ed2e66be1ccc8dbe01aeb7297181a40a
diego olivera <2024_diego.olivera@iticbcn.cat>
2024-12-10 17:12:23 +0100

    Working act.6

--- ACTIVITAT_9/body-nested.py
+++ ACTIVITAT_9/body-nested.py
@@ -1,10 +1,17 @@
 from typing import List, Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
-
+import psycopg2
+from psycopg2.extras import RealDictCursor
 app = FastAPI()
 
+class User(BaseModel):
+    id: int
+    name: str
+    email: str
+
+    class Config:
+        orm_mode = True
 
 class Item(BaseModel):
     name: str
@@ -13,8 +20,28 @@ class Item(BaseModel):
     tax: Union[float, None] = None
     tags: List[str] = []
 
+def get_db():
+    conn = psycopg2.connect(
+        dbname="diego",
+        user="postgres",
+        password="admin",
+        host="localhost",
+        port="5432",
+        cursor_factory=RealDictCursor
+    )
+    try:
+        yield conn
+    finally:
+        conn.close()
 
 @app.put("/items/{item_id}")
 async def update_item(item_id: int, item: Item):
     results = {"item_id": item_id, "item": item}
     return results
+
+@app.get("/users/", response_model=List[User])
+def read_users(db=Depends(get_db)):
+    with db.cursor() as cursor:
+        cursor.execute("SELECT id, name, email FROM users")
+        users = cursor.fetchall()
+    return users
